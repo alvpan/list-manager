3761ede59364f84c1c01ae8d5e659449cb655ed7
alvpan <pan.alvanopoulos@gmail.com>
2025-04-29 10:19:14 +0300

    Added Google Analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -179,6 +179,26 @@ export default function Home() {
     document.documentElement.style.padding = "0";
   }, []);
 
+  // Google Analytics
+  useEffect(() => {
+
+    // Load gtag library
+    const script1 = document.createElement("script");
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=G-SC4R1WN19F`;
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    // Initialize Analytics
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-SC4R1WN19F', { send_page_view: true });
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
   return (
     //BG
     <div
